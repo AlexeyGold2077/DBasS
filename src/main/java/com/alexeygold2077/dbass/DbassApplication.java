9ce9d8b19bc8000c05126ad8e0fe1eee7a3f8581
Alexey Zolotarev <alexgold2077@yandex.ru>
2025-01-10 21:56:16 +0300

    add basic code from spring boot initializer

---
+++ src/main/java/com/alexeygold2077/dbass/DbassApplication.java
@@ -0,0 +1,11 @@
+package com.alexeygold2077.dbass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbassApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DbassApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexeygold2077/dbass/DbassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexeygold2077.dbass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
